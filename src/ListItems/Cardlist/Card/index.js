7dbca23ac81c07c06eb8e55297ea302c63453b95
Dot-Rhs <rhysh1986@gmail.com>
2023-08-13 22:30:18 +0100

    Basically each ingredient was a seperate entry
    You could deconstruct each ingredient (1-15), That would be a lot of repetive code though.
    
    So I've used Object.entries to create an array of each key/value in the object.
    
    I then filter for each key that includes "strIngredient" in it.
    You then map through each returned array (each key/val) and return the value
    
    You then pass that array to Card and map through each one

--- src/ListItems/Cardlist/Card/index.js
+++ src/ListItems/Cardlist/Card/index.js
@@ -5,7 +5,7 @@ const Card = ({ key, image, header, recipe, items, measures }) => {
       <img src={image} height="100px" width="100px" />
       <h2>{header}</h2>
       <h3>{recipe}</h3>
-      <h3>{items}</h3>
+      {items.map(ingredient => <h3>{ingredient}</h3>)}
       <h3>{measures}</h3>
     </div>
   );
