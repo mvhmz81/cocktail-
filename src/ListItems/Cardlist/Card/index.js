3a3df5a98e8acdd7ac8c24772b91c0bb29bc95be
mvhmz81 <mvhmz@yahoo.co.uk>
2023-08-14 12:32:32 +0100

    ammended card code to allow measure and ingriedint both to sho

--- src/ListItems/Cardlist/Card/index.js
+++ src/ListItems/Cardlist/Card/index.js
@@ -1,12 +1,26 @@
-const Card = ({ key, image, header, recipe, items, measures }) => {
-  console.log();
+// const Card = ({ key, image, header, recipe, items, measures }) => {
+//   console.log();
+//   return (
+//     <div>
+//       <img src={image} height="100px" width="100px" />
+//       <h2>{header}</h2>
+//       <h3>{recipe}</h3>
+//       {items.map(ingredient => <h3>{ingredient}</h3>)}
+//       <h3>{measures}</h3>
+//     </div>
+//   );
+// };
+const Card = ({ image, header, items, recipe }) => {
   return (
-    <div>
-      <img src={image} height="100px" width="100px" />
-      <h2>{header}</h2>
-      <h3>{recipe}</h3>
-      {items.map(ingredient => <h3>{ingredient}</h3>)}
-      <h3>{measures}</h3>
+    <div className="card">
+      <h2 className="card-header">{header}</h2>
+      <img src={image} alt={header} className="card-image" />
+      <ul className="card-items">
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <p className="card-recipe">{recipe}</p>
     </div>
   );
 };
--- src/ListItems/Cardlist/index.js
+++ src/ListItems/Cardlist/index.js
@@ -7,17 +7,22 @@ const Cardlist = ({ getCocktailData }) => {
   return (
     <div>
       {drinks.map((cocktail) => {
-        console.log('indeg: ', cocktail);
-        const ingredients = Object.entries(cocktail).filter(entries => {
-          const [key, value] = entries
-          if (key.includes('strIngredient')) return value
-          
-        }).map(ingredients => {
-          const [key, value] = ingredients
-
-          return value})
-
-        console.log(ingredients)
+        //       console.log("indeg: ", cocktail);
+        const ingredients = Object.entries(cocktail).reduce(
+          (acc, [key, value]) => {
+            if (key.includes("strIngredient") && value) {
+              const measureKey = key.replace("strIngredient", "strMeasure");
+              const measure = cocktail[measureKey];
+              //acc.push(`${measure} ${value}`); //if value null looks bad so add ternary that if measure falsy return statement
+              const ingredientWithMeasure = measure
+                ? `${measure} ${value}`
+                : `${value} to taste`;
+              acc.push(ingredientWithMeasure);
+            }
+            return acc;
+          },
+          []
+        );
 
         return (
           <Card
@@ -25,7 +30,7 @@ const Cardlist = ({ getCocktailData }) => {
             image={cocktail.strDrinkThumb}
             header={cocktail.strDrink}
             items={ingredients} //how to get multiple strIngredient1
-            measures={cocktail.strMeasure1} //how to get multiple strMeasure1
+            //           measures={cocktail.strMeasure1} //how to get multiple strMeasure1
             recipe={cocktail.strInstructions}
           />
         );
