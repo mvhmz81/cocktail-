2b687a7601522767f8de73b76ca3da3c73876bc0
mvhmz81 <mvhmz@yahoo.co.uk>
2023-08-25 16:53:57 +0100

    h1/2 not responsive

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <div className="header-continer">
+      <div className="header-container">
         <div className="title">
           <h1>MixologyHub: </h1>
           <h2>Cocktail Recipes & Ingredients</h2>
